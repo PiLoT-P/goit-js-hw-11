746df2b06aba5ec48ebf7699a3d2edb368e9a403
PiLoT-P <59173880+PiLoT-P@users.noreply.github.com>
2023-03-13 19:33:04 +0200

    update

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ const getImageFromServer = new GetImageFromServer();
 let nameImage = '';
 let totalNumberImage = 0;
 let lightbox = null;
+let totalPages = 0;
+let page = getImageFromServer.page;
 
 
 async function addImages(event) {
     event.preventDefault();
 
-
     listImage.innerHTML = '';
-    getImageFromServer.page = 1;
 
     const {
         elements: { searchQuery }
@@ -29,11 +29,13 @@ async function addImages(event) {
 
     if (!nameImage) {
         buttonLoadMor.classList.add('is-hiden');
+        ulTag.innerHTML = '';
         return;
     }
 
     try {
         const getImg = await getImageFromServer.getImages(nameImage);
+        console.log(getImg);
         if (getImg.totalHits <= 0) {
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
             return;
@@ -42,19 +44,98 @@ async function addImages(event) {
         Notiflix.Notify.success(`Hooray! We found ${getImg.totalHits} images.`);  
         createListItem(getImg.hits);
         buttonLoadMor.classList.remove('is-hiden');
+        // console.log(getImg.totalHits/getImg.hits.length);
+        totalPages = Math.round(getImg.totalHits / getImg.hits.length);
     } catch (error) {
         console.log(error.message);
     }
 
     lightbox = new SimpleLightbox('.gallery a', {});
+    element(totalPages, page);
 }
 
 formImages.addEventListener('submit', addImages);
 
-async function onLoadMore(event) {
-    getImageFromServer.page += 1;
+const ulTag = document.querySelector('.list');
+
+function element(totalPages, page) {
+    console.log('listtop',page);
+    let liTag = '';
+    let thirdPagesAnd = totalPages - 2;
+    let thirdPages = page - 2;
+    let curentPage = page;
+
+    if (curentPage >= 1) {
+        if (curentPage === 1) {
+            liTag += `<li class="pagination-arrow"><button disabled="true" class="prew-button" type="button">prew</button></li>`;
+        } else {
+            liTag += `<li class="pagination-arrow"><button class="prew-button" type="button">prew</button></li>`;
+        }
+    }
+    if (totalPages > 6) {
+        if (page < 3) {
+            thirdPages = 1;
+            page = 3;
+        }
+        if (page > totalPages - 3) {
+            page = totalPages - 3;
+            thirdPages = page - 2;
+        }
+        for (let i = thirdPages; i <= page; i++) {
+            if (i === curentPage) {
+                liTag += `<li class="pagination-number"><button class="color" type="button">${i}</button></li>`;
+            } else {
+                liTag += `<li class="pagination-number"><button type="button">${i}</button></li>`;
+            }
+        }
+        liTag += `<span>...</span>`;
+        for (let i = thirdPagesAnd; i <= totalPages; i++) {
+            if (i === curentPage) {
+                liTag += `<li class="pagination-number"><button class="color" type="button">${i}</button></li>`;
+            } else {
+                liTag += `<li class="pagination-number"><button type="button">${i}</button></li>`;
+            }
+        }
+    } else {
+        for (let i = page; i <= totalPages; i++) {
+            if (i = page) {
+                liTag += `<li class="pagination-number"><button class="color" type="button">${i}</button></li>`;
+            } else {
+                liTag += `<li class="pagination-number"><button type="button">${i}</button></li>`;
+            }
+        }
+    }
+    if (curentPage <= totalPages) {
+        if (curentPage === totalPages) {
+            liTag += `<li class="pagination-arrow"><button disabled="true" class="next-button" type="button">next</button></li>`;
+        } else {
+            liTag += `<li class="pagination-arrow"><button class="next-button" type="button">next</button></li>`;
+        }
+    }
+    ulTag.innerHTML = liTag;
+    
+}
+
+async function loadMor(event) {
+    if (page < 1) {
+        page = 0;
+    }
+    if (page > totalPages) {
+        page = totalPages;
+    }
+    if (event.target.classList.contains('next-button')) {
+        page += 1;
+        getImageFromServer.page = page;
+    } else if (event.target.classList.contains('prew-button')) {
+        page -= 1;
+        getImageFromServer.page = page;
+    } else {
+        getImageFromServer.page = Number(event.target.textContent);
+        page = getImageFromServer.page;
+    }
 
     try {
+        console.log("getpage",getImageFromServer.page);
         const getImg = await getImageFromServer.getImages(nameImage);
         totalNumberImage += getImg.hits.length;
         if (totalNumberImage >= getImg.totalHits) {
@@ -67,6 +148,9 @@ async function onLoadMore(event) {
         console.log(error.message);
     }
     lightbox.refresh();
+    console.log(event.target.classList.contains('next-button'));
+    console.log('page',page);
+    element(totalPages, page);
 }
 
-buttonLoadMor.addEventListener('click', onLoadMore);
+ulTag.addEventListener('click', loadMor);
